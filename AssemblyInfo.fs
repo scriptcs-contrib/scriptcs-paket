e86f74688f5837576e08d89a1b67df6ca5535b80
filipw <wojcieszyn@gmail.com>
2014-11-09 23:29:00 +0100

    first outline commit

---
+++ AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ScriptCs.Paket")>]
+[<assembly: AssemblyProductAttribute("ScriptCs.Paket")>]
+[<assembly: AssemblyDescriptionAttribute("A module allowing you to use Paket with scriptcs (instead of Nuget)")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
---
+++ Library1.fs
@@ -0,0 +1,41 @@
+﻿namespace ScriptCs.Paket
+
+open System
+open ScriptCs
+open ScriptCs.Hosting
+open ScriptCs.Contracts
+open Paket
+
+type PaketInstallationProvider(fileSystem : IFileSystem) =
+    let fs = fileSystem
+    let dep = Dependencies.Locate(fs.CurrentDirectory)
+
+    interface IInstallationProvider with
+        member x.GetRepositorySources(path) =
+            [fs.GetWorkingDirectory(path)] |> Seq.ofList
+
+        member x.Initialize() =
+            ()
+
+        member x.InstallPackage(packageId, allowPreRelease) =
+            dep.Add(packageId.PackageId, packageId.Version.ToString())
+            dep.Install(false, false)
+
+        member x.IsInstalled(packageId, allowPreRelease) =
+            dep.FindReferencesFor(packageId.PackageId) |> Seq.exists (fun n -> n = packageId.PackageId)
+
+type PaketAssemblyResolver =
+    interface IAssemblyResolver with
+        member x.GetAssemblyPaths(path, binariesOnly) =
+            Seq.empty
+
+[<Module("paket")>]
+type FSharpModule () =
+    interface IModule with
+        member x.Initialize(builder) =
+            builder.
+                InstallationProvider<PaketInstallationProvider>().
+                AssemblyResolver<PaketAssemblyResolver>() 
+                |> ignore
+                
+
